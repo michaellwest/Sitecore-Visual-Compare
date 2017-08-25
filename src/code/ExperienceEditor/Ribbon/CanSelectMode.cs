962a9da2880989ee6b91809f5a7fa04a11f711be
Kamruz Jaman <jammykam@gmail.com>
2017-08-25 19:14:01 -0400

    Updated to to add Visual Compare to Mode Switcher list in Experience Editor. Added installable package for Sitecore 8.1+ General project cleanup.

---
+++ src/code/ExperienceEditor/Ribbon/CanSelectMode.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.ExperienceEditor.Utils;
+using Sitecore.SecurityModel;
+
+namespace Sitecore.SharedSource.VisualCompare.ExperienceEditor.Ribbon
+{
+    public class CanSelectMode : Sitecore.ExperienceEditor.Speak.Ribbon.Requests.SelectMode.CanSelectMode
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            Assert.IsNotNullOrEmpty(this.RequestContext.Value, "Could not get string value for requestArgs:{0}", (object)this.Args.Data);
+            string mode = string.Empty;
+            using (new SecurityDisabler())
+            {
+                Item obj = Client.CoreDatabase.GetItem(new ID(HttpUtility.UrlDecode(this.RequestContext.Value)));
+                if (obj != null)
+                    mode = obj["Message"];
+            }
+            switch (mode)
+            {
+                case "sharedsource:system:compare":
+                    return new PipelineProcessorResponseValue()
+                    {
+                        Value = WebEditUtility.CanPreview()
+                    };
+                default:
+                    return base.ProcessRequest();
+            }
+        }
+    }
+}
--- src/code/Pipelines/RenderField/CustomRenderFieldArgs.cs
+++ src/code/Pipelines/RenderField/CustomRenderFieldArgs.cs
@@ -1,6 +1,6 @@
 ﻿using Sitecore.Pipelines.RenderField;
 
-namespace Sitecore.Sharedsource.Pipelines.RenderField
+namespace Sitecore.Sharedsource.VisualCompare.Pipelines.RenderField
 {
     // to avoid infinite recursion, this class indicates when 
     // a renderField processor calls the renderField pipeline
